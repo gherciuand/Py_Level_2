7c2358ffad71fe49801690d9b1b7ed2dccfe07d6
gherciuand <68415760+gherciuand@users.noreply.github.com>
2020-11-14 23:15:50 +0200

    Create app.py

---
+++ HW_2_proxy_images_procesor/app.py
@@ -0,0 +1,33 @@
+# watcher / observer / listener #####
+from os import path, listdir
+from time import sleep
+
+extensii_acceptate = [".jpg", ".jpeg", ".png"]
+
+
+def observe(directory):
+    if not path.isdir(directory):
+        print("ERROR - directory not found")
+        return
+
+    def img(directory):
+        imglist = []
+        for file in listdir(directory):
+            if path.isdir(directory + f"/{file}") is not True:
+                if path.splitext(file)[1] in extensii_acceptate:
+                    imglist.append(file)
+        return imglist
+
+    lastFileList = img(directory)
+
+    while True:
+        fileList = img(directory)
+        if lastFileList != fileList:
+
+
+            print(" ceva schimbari")
+
+        lastFileList = fileList
+        sleep(0.5)
+
+observe("./images")
