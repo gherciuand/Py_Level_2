7a82fb0a5d56a1e5fb3e7e7e089110e06f3453ef
gherciuand <68415760+gherciuand@users.noreply.github.com>
2020-12-23 03:42:51 +0200

    Update project.py

--- HW_5_Chat/project.py
+++ HW_5_Chat/project.py
@@ -2,4 +2,4 @@ import subprocess
 
 print('starting project...')
 print("SPAWING client process")
-subprocess.run(["./client/app.py"])
+subprocess.run(['D:/Pers/Python/python.exe', "./client/app.py"])
