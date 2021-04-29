26a4c162248e4d2345fce7e5d1f0c2775e4e3ff0
gherciuand <68415760+gherciuand@users.noreply.github.com>
2021-04-30 02:37:02 +0300

    Update images.py

--- face_detection/images.py
+++ face_detection/images.py
@@ -12,8 +12,8 @@ def saveImage(url, name):
     if not os.path.exists(f'{IMAGE_BASE_FOLDER}/{name}'):
         os.mkdir(f'{IMAGE_BASE_FOLDER}/{name}')                             # creating a new folder if it does not exist
     if len(list(Path(f'{IMAGE_BASE_FOLDER}/{name}').glob('*.jpg'))) == 0:   # check if folder contain image file
-        save_img = urllib.request.urlretrieve(url, f'{IMAGE_BASE_FOLDER}/{name}/0000.jpg')  # save first image
+        urllib.request.urlretrieve(url, f'{IMAGE_BASE_FOLDER}/{name}/0000.jpg')  # save first image
     else:
         last_img = list(Path(f'{IMAGE_BASE_FOLDER}/{name}').glob('*.jpg'))[-1].stem
         next_img = (last_img.join(['{0:04}'.format((int(last_img) + 1))]))  #set name for the next image
-        save_img = urllib.request.urlretrieve(url, f'{IMAGE_BASE_FOLDER}/{name}/{next_img}.jpg') # save next image
+        urllib.request.urlretrieve(url, f'{IMAGE_BASE_FOLDER}/{name}/{next_img}.jpg') # save next image
