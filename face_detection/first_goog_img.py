ac426bb482b913d7f29bec6fd7922e7886661f4b
gherciuand <68415760+gherciuand@users.noreply.github.com>
2021-04-30 02:08:32 +0300

    update
    update

--- face_detection/first_goog_img.py
+++ face_detection/first_goog_img.py
@@ -2,17 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import urllib.request
 from time import sleep
+from images import saveImage
 
 name = input('Find :')
 
-browser = webdriver.Chrome("chromedriver_win32/chromedriver.exe")
+browser = webdriver.Chrome("drivers/chromedriver.exe")
 browser.get('https://images.google.com/')
 inputElement = browser.find_element_by_class_name('gLFyf')
 inputElement.send_keys(name)
 inputElement.send_keys(Keys.ENTER)
 data_image = browser.find_element_by_class_name("rg_i")
-scr = data_image.get_attribute("src")
-
-img = urllib.request.urlretrieve(scr, f'./orig_img_dir/{name}.jpeg')
-
+src = data_image.get_attribute("src")
+# print(src)
+# img = urllib.request.urlretrieve(src, f'./orig_img_dir/{name}.jpg')
+# print(img)
 browser.quit()
+
+saveImage(src, name)
---
+++ face_detection/images.py
@@ -0,0 +1,19 @@
+# from tkinter import Tk, filedialog
+import os
+import urllib.request
+from pathlib import Path
+
+# root = Tk().withdraw()
+# IMAGE_BASE_FOLDER = filedialog.askdirectory()
+IMAGE_BASE_FOLDER = './orig_img_dir'
+
+
+def saveImage(url, name):
+    if not os.path.exists(f'{IMAGE_BASE_FOLDER}/{name}'):
+        os.mkdir(f'{IMAGE_BASE_FOLDER}/{name}')                             # creating a new folder if it does not exist
+    if len(list(Path(f'{IMAGE_BASE_FOLDER}/{name}').glob('*.jpg'))) == 0:   # check if folder contain image file
+        save_img = urllib.request.urlretrieve(url, f'{IMAGE_BASE_FOLDER}/{name}/0000.jpg')  # save first image
+    else:
+        last_img = list(Path(f'{IMAGE_BASE_FOLDER}/{name}').glob('*.jpg'))[-1].stem
+        next_img = (last_img.join(['{0:04}'.format((int(last_img) + 1))]))  #set name for the next image
+        save_img = urllib.request.urlretrieve(url, f'{IMAGE_BASE_FOLDER}/{name}/{next_img}.jpg') # save next image
